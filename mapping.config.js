761ceef90973c376496db8d128d35978a6d567ff
angular-moon <xiaoxuss@163.com>
2015-07-08 20:17:35 +0800

    1

---
+++ mapping.config.js
@@ -0,0 +1,13 @@
+module.exports = function(options) {
+
+  // options 为 components.json 的值。
+
+  // 可以添加一些逻辑，最终返回一个 mapping 配置列表。
+  return [
+    {
+      reg: /^\/README\.md$/i,
+      release: false
+    }
+  ];
+
+}
